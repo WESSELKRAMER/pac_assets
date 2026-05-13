e094109bb51670d8aa9c9f9507cd324655a99672
WESSELKRAMER <info@wesselkramer.nl>
2026-05-13 13:00:23 +0200

    Update swipers.js

--- swipers.js
+++ swipers.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".stories-swiper").forEach((swiperEl) => {
+    new Swiper(swiperEl, {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      speed: 700,
 
-  const swiper = new Swiper(".stories-swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    speed: 700,
-    grabCursor: true,
+      grabCursor: true,
+      simulateTouch: true,
+      allowTouchMove: true,
+      touchStartPreventDefault: false,
+      watchOverflow: false,
 
-    freeMode: {
-      enabled: true,
-      momentum: true,
-    },
-
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-    },
-
+      freeMode: {
+        enabled: true,
+        momentum: true
+      }
+    });
   });
-
 });
