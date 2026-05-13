4dfd45b1ce44e4f9e6533738b3563cbfb518fbb3
WESSELKRAMER <info@wesselkramer.nl>
2026-05-13 12:28:25 +0200

    Update swipers.js

--- swipers.js
+++ swipers.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const swiper = new Swiper(".stories-swiper", {
     slidesPerView: "auto",
     spaceBetween: 16,
     speed: 700,
     grabCursor: true,
+
     freeMode: {
       enabled: true,
       momentum: true,
-      momentumRatio: 0.8,
     },
+
     keyboard: {
       enabled: true,
       onlyInViewport: true,
     },
+
   });
+
 });
